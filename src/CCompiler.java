347323d28aa5b0d216575e6e156d47904dc32f9a
Hepheir <hepheir@gmail.com>
2023-11-13 21:17:35 +0900

    feat(CCompiler): Compiler 인터페이스의 C언어 버전 구현체 생성

---
+++ src/CCompiler.java
@@ -0,0 +1,13 @@
+public class CCompiler implements Compiler {
+    final String INFO_MSG_COMPILING = "Compiling code: %s\n";
+    final String INFO_MSG_GENERATING = "Generating object code: %s\n";
+
+    @Override
+    public ObjectCode compile(SourceCode sourceCode) {
+        ObjectCode objectCode;
+        System.out.printf(INFO_MSG_COMPILING, sourceCode.getFileName());
+        objectCode = new ObjectCode(sourceCode.getFileName());
+        System.out.printf(INFO_MSG_GENERATING, objectCode.getFileName());
+        return objectCode;
+    }
+}
