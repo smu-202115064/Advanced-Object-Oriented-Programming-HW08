8e3e8983090d34f542a4099882f32bf7d8b50d90
Hepheir <hepheir@gmail.com>
2023-11-13 21:25:19 +0900

    feat(CLinker): Linker 인터페이스의 C언어 버전 구현체 생성

---
+++ src/CLinker.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class CLinker implements Linker {
+    final String INFO_MSG_LINKING = "Linking\n";
+
+    @Override
+    public Executable link(String exeFileName, ObjectCode[] objFiles) {
+        return link(exeFileName, Arrays.asList(objFiles));
+    }
+
+    @Override
+    public Executable link(String exeFileName, List<ObjectCode> objFiles) {
+        System.out.print(INFO_MSG_LINKING);
+        StringBuilder sb = new StringBuilder();
+        for (ObjectCode objectCode : objFiles) {
+            sb.append('\n').append(objectCode.getFileName());
+        }
+        return new Executable(exeFileName, sb.toString());
+    }
+}
