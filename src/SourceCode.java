85ad966acbe3ae8f26d42fe0117174f6dd25d6fa
Hepheir <hepheir@gmail.com>
2023-11-13 20:50:52 +0900

    feat(SourceCode): SourceCode 클래스 구현

---
+++ src/SourceCode.java
@@ -0,0 +1,14 @@
+public class SourceCode {
+    private String fileName;
+
+    public SourceCode(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
