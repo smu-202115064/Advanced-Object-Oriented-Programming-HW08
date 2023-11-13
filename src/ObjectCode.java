3c0138bdf422f813a090f1c3eb8cba7a0dcd059f
Hepheir <hepheir@gmail.com>
2023-11-13 20:51:31 +0900

    feat(ObjectCode): ObjectCode 클래스 구현

---
+++ src/ObjectCode.java
@@ -0,0 +1,14 @@
+public class ObjectCode {
+    private String fileName;
+
+    public ObjectCode(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
